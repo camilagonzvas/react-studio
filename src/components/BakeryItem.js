e640f756021a5ae3bdc9b22fde05d79688f132e0
Camila Gonzalez Vasquez <90671009+camilagonzvas@users.noreply.github.com>
2022-11-13 15:25:39 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,11 @@
 // TODO: create a component that displays a single bakery item
+export function BakeryItem({item, handleClick}) {
+    return (
+        <div>
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            <img src = {item.image}></img>
+            <button onClick={() => handleClick(item.price, item.name)}>Click me!</button>
+        </div>
+    )
+}
